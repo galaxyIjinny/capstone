aeaba2c0fcd7b311112f1262862fd4787d07ddf6
ljw43 <122363513+ljw43@users.noreply.github.com>
2023-01-17 16:46:07 +0900

    Create Nav.js

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { FiSearch } from "react-icons/fi";
+import "./Nav.css";
+const Nav = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar_logo">
+        <Link to="/">Brandnew</Link>
+      </div>
+      <ul className="navbar_menu">
+        <li>
+          <Link to="domesticmark">국내상표등록</Link>
+        </li>
+        <li>
+          <Link to="overseasmark">해외상표등록</Link>
+        </li>
+        <li>
+          <Link to="search">상표검색</Link>
+        </li>
+        <li>
+          <Link to="price">비용안내</Link>
+        </li>
+        <li>
+          <Link to="step">절차안내</Link>
+        </li>
+        <li>
+          <Link to="qna">Q&A</Link>
+        </li>
+      </ul>
+      <ul className="navbar_search">
+        <li>
+          <input type="text" placeholder="검색할 상표를 입력해주세요" />
+        </li>
+        <li>
+          <button>
+            <FiSearch style={{ fontSize: "1.3rem" }} />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
