cead8ba9dfb712b0d5bd2d70c9b7a1c5e259b525
ljw43 <leejiwon_tech@naver.com>
2023-01-21 20:26:58 +0900

    Update Main.js
    버튼 상호 작용 추가

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,20 +1,27 @@
 import { ReactComponent as WorkTime } from "../../assets/images/WorkTime.svg";
 import { HiArrowRight } from "react-icons/hi";
+import { Link } from "react-router-dom";
 import "./Main.css";
 const Main = () => {
   return (
     <div className="main">
       <WorkTime width={1000} height={850} />
-      <div className="div">
+      <div className="container">
         <div className="title">든든하고 합리적인 상표등록</div>
-        <button id="overseas">
-          해외상표등록
-          <HiArrowRight style={{ fontSize: "1.3rem" }} />
-        </button>
-        <button id="domestic">
-          국내상표등록
-          <HiArrowRight style={{ fontSize: "1.3rem" }} />
-        </button>
+        <Link to="/domesticmark">
+          <button id="domestic">
+            국내상표등록
+            <HiArrowRight style={{ fontSize: "1.3rem" }} />
+          </button>
+        </Link>
+        <Link to="overseasmark">
+          <button id="overseas">
+            해외상표등록
+            <HiArrowRight style={{ fontSize: "1.3rem" }} />
+          </button>
+        </Link>
+      </div>
+      <div>
         <div className="statistics">
           <div className="mark"></div>
           <div className="search"></div>
