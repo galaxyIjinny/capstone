c2de28f710a47d255bf66d0edf0857e96161d328
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2023-02-07 15:30:19 +0900

    상표카드
    .

--- src/components/domesticMark/DomesticMark.js
+++ src/components/domesticMark/DomesticMark.js
@@ -4,6 +4,38 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Avatar } from '@mui/material';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { useState } from 'react';
+import { ReactComponent as starbucks } from "../../assets/images/starbucks.svg"; //복합상표
+import { ReactComponent as apple } from "../../assets/images/apple.svg"; //도형상표
+import { ReactComponent as uahanbrothers } from "../../assets/images/uahanbrothers.svg"; //문자상표
+
+function MarkAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={ apple }
+          alt="상표"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            복합상표
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           상표유형
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
 
 function DomesticMark(){
   return(
@@ -12,18 +44,20 @@ function DomesticMark(){
         <br/><br/><br/>
         <Container>
           <div>
-            <Grid container spacing={2} style={{textAlign:'left'}}>
-                  <Grid item xs={12}> <h3>1. 상표유형</h3> </Grid>
-                  <Grid item xs={3}> <Button variant="outlined">문자 상표</Button> </Grid>
-                  <Grid item xs={3}> <Button variant="outlined">도형 상표</Button> </Grid>
-                  <Grid item xs={3}> <Button variant="outlined">복합 상표</Button> </Grid>
-                  <Grid item xs={3}> <Button variant="outlined">기타 & 모르겠음</Button> </Grid>              
+            <Grid container spacing={4} style={{textAlign:'left'}}>
+                  <Grid item xs={12}> <b className='littleTitle'>1. 상표유형</b></Grid>
+                  <Grid item xs={3}> <MarkAreaCard/> </Grid>
+                  <Grid item xs={3}> <MarkAreaCard/> </Grid>
+                  <Grid item xs={3}> <MarkAreaCard/> </Grid>
+                  <Grid item xs={3}> <MarkAreaCard/> </Grid>                         
               <br/><br/><br/><br/><br/>
-              <Grid item xs={12}> <h3>2. 상표명</h3> </Grid> 
+
+              <Grid item xs={12}> <b className='littleTitle'>2. 상표명</b>  </Grid> 
               <Grid item xs={12}><TextField id="info-markName"
               fullWidth label="상표명을 입력하세요." variant="outlined"/></Grid> 
               <br/><br/><br/><br/><br/>
-              <Grid item xs={12}> <h3>3. 참고용 설문 *고객님의 사업을 이해하고자 하는 추가 질문입니다.</h3> </Grid>
+              <Grid item xs={12}>
+              <b className='littleTitle'>3. 참고용 설문 <small><u> *고객님의 사업을 이해하고자 하는 추가 질문입니다.</u></small></b></Grid>
               <Grid item xs={12}><TextField id="info-markIntroduce"
               fullWidth label="예시)와인을 판매하는 주류점입니다, 전자기기 기업의 스마트폰 브랜드입니다." 
               variant="outlined"/></Grid>
@@ -41,6 +75,7 @@ function DomesticMark(){
           <div>
             <Grid item xs={12}> <h3 style={{textAlign:'left'}}>주문확인</h3> </Grid>
           </div>
+          
           </Container>
       </div>
     </div>
