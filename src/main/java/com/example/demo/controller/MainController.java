c88864ec947228053af0b020059b55cc3daa2c57
galaxyIjinny <yahyunjin5@naver.com>
2023-03-08 09:35:18 +0900

    radio button, param?

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -8,10 +8,7 @@ import com.example.demo.service.PersonalApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MainController {
@@ -29,19 +26,15 @@ public class MainController {
     public String markApplication() {
         return "/main/markApplication";
     }
-    @PostMapping("main/application/personal")
-    public ResponseEntity<PersonalApplicationDto> createPersonalApplication(@RequestBody PersonalApplicationDto dto) {
-        PersonalApplicationDto createdDto = personalApplicationService.create(dto);
-        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
+    @PostMapping("main/application")
+    public String typeApplication(@RequestParam("applicantType") String applicantType) { // 라디오 버튼 이용
+        if (applicantType.equals("personal")) {  // 방식 고민
+            return "/main/personalApplication";
+        } else if (applicantType.equals("corporate")) {
+            return "/main/corporateApplication";
+        } else
+            return "/main/markApplication";
     }
-
-    @PostMapping("main/application/corporate")
-    public ResponseEntity<CorporateApplicationDto> createCorporateApplication(@RequestBody CorporateApplicationDto dto) {
-        CorporateApplicationDto createdDto = corporateApplicationService.create(dto);
-        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
-    }
-
-
 
     @GetMapping("/main/processInfo") // 절차안내
     public String processInfo() {
