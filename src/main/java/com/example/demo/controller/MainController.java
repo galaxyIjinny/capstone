99abe5c164b10aebd02945494c6d6c417af1d377
galaxyIjinny <yahyunjin5@naver.com>
2023-03-27 12:02:05 +0900

    modify merge

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,31 +1,40 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.CorporateDto;
-import com.example.demo.dto.MarkDto;
-import com.example.demo.dto.PersonalDto;
-import com.example.demo.dto.UserDto;
-import com.example.demo.service.CorporateService;
-import com.example.demo.service.MarkService;
-import com.example.demo.service.PersonalService;
-import com.example.demo.service.RegisterService;
-import com.example.demo.service.UserService;
+import com.example.demo.dto.*;
+import com.example.demo.service.corporate.CorporateServiceImpl;
+import com.example.demo.service.info.InfoServiceImpl;
+import com.example.demo.service.mark.MarkServiceImpl;
+import com.example.demo.service.personal.PersonalServiceImpl;
+import com.example.demo.service.user.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/main")
+@RequestMapping("/api/main")
 public class MainController {
     @Autowired
-    private MarkService markService;
+    private InfoServiceImpl infoService;
     @Autowired
-    private PersonalService personalService;
+    private MarkServiceImpl markService;
     @Autowired
-    private CorporateService corporateService;
+    private PersonalServiceImpl personalService;
     @Autowired
-    private RegisterService registerService;
-    @GetMapping("/register")
+    private CorporateServiceImpl corporateService;
+    @Autowired
+    private UserServiceImpl userService;
+
+    /*@Autowired
+    private RegisterService registerService;*/
+
+
+
+    @GetMapping("/info")
+    public List<InfoDto> showAllInfo() {
+        return infoService.info();
+    }
+    @GetMapping("/mark")
     public List<MarkDto> showMarks() {
         return markService.marks();
     }
@@ -41,48 +50,4 @@ public class MainController {
     public List<UserDto> showUsers() {
         return userService.user();
     }
-
-
-
-
-
-
-
-    /*@GetMapping("/main") // 메인 페이지
-    public String markMain() {
-        return "/main";
-    }
-    @GetMapping("/main/application") // 상표 등록 신청
-    public String markApplication() {
-        return "/main/markApplication";
-    }
-    @PostMapping("main/application")
-    public String typeApplication(@RequestParam("applicantType") String applicantType) { // 라디오 버튼 이용
-        if (applicantType.equals("personal")) {  // 방식 고민
-            return "/main/personalApplication";
-        } else if (applicantType.equals("corporate")) {
-            return "/main/corporateApplication";
-        } else
-            return "/main/markApplication";
-    }
-
-    @GetMapping("/main/processInfo") // 절차안내
-    public String processInfo() {
-        return "/main/processInfo";
-    }
-
-    @GetMapping("/main/costInfo") // 비용안내
-    public String costInfo() {
-        return "/main/costInfo";
-    }
-
-    @GetMapping("/main/corporateInfo") // 회사소개
-    public String corporateInfo() {
-        return "/main/corporateInfo";
-    }
-
-    @GetMapping("/main/faq") // FAQ
-    public String faq() {
-        return "/main/processInfo";
-    }*/
 }
--- src/main/java/com/example/demo/service/corporate/CorporateServiceImpl.java
+++ src/main/java/com/example/demo/service/corporate/CorporateServiceImpl.java
@@ -1,5 +1,4 @@
-package com.example.demo.service;
-
+package com.example.demo.service.corporate;
 import com.example.demo.dto.CorporateDto;
 import com.example.demo.entity.Corporate;
 import com.example.demo.entity.Mark;
@@ -11,25 +10,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
-
 @Slf4j
 @Service
-public class CorporateService {
+public class CorporateServiceImpl implements CorporateService {
     @Autowired
     private CorporateRepository corporateRepository;
     @Autowired
     private MarkRepository markRepository;
-
+    @Override
     public List<CorporateDto> corporate() {
         return corporateRepository.findAll().stream()
                 .map(CorporateDto::createCorporateDto)
                 .collect(Collectors.toList());
     }
+    @Override
     @Transactional
     public CorporateDto create(Long mid, CorporateDto dto) {
-        Mark mark = markRepository.findById(mid).orElseThrow(() -> new IllegalArgumentException("error"));
+        Mark mark = markRepository.findById(mid)
+                .orElseThrow(() -> new IllegalArgumentException("error"));
         Corporate corporate = Corporate.createCorporate(dto, mark);
         Corporate created = corporateRepository.save(corporate);
         return CorporateDto.createCorporateDto(created);
+    }
+
+    @Override
+    public CorporateDto update(Long id, CorporateDto dto) {
+        Corporate target = corporateRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("error"));
+        target.patch(dto);
+        Corporate updated = corporateRepository.save(target);
+        return CorporateDto.createCorporateDto(updated);
+    }
+
+    @Override
+    public CorporateDto delete(Long id) {
+        Corporate target = corporateRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("error"));
+        corporateRepository.delete(target);
+        return CorporateDto.createCorporateDto(target);
     }
 }
