ff49e7f8abe5ef6ba365b2b37cd23ab1c4a8f450
ljw43 <leejiwon_tech@naver.com>
2023-04-03 07:05:55 +0900

    Edit Navbar Component
    페이지별로 backgroundColor, boderBottom 변경 기능 추가

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,12 @@
-import {
-  Box,
-  Container,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
+import { Box, Drawer, List, ListItem, ListItemText } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { styled } from "@mui/system";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./Navbar.scss";
 import logoImg from "../../assets/images/logo/복합.svg";
-const Navbar = () => {
+
+const Navbar = ({ backgroundColor, borderBottom }) => {
   const [open, setOpen] = useState(false);
 
   const toggleDrawer = (open) => (event) => {
@@ -25,30 +19,13 @@ const Navbar = () => {
 
     setOpen(open);
   };
-  const list = () => (
-    <Box
-      sx={{ width: "600px" }}
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
-    >
-      <ul className="nav">
-        {[
-          "신 청 내 역",
-          "비 용 안 내",
-          "절 차 안 내",
-          "회 사 소 개",
-          "등 록 후 기",
-          "고 객 문 의",
-        ].map((text) => (
-          <li>{text}</li>
-        ))}
-      </ul>
-      <ul className="company">
-        <li>계좌</li>
-        <li>고객문의</li>
-      </ul>
-    </Box>
-  );
+
+  const menuItems = [
+    { text: "비용안내", link: "/price" },
+    { text: "절차안내", link: "/procedure" },
+    { text: "회사안내", link: "/about" },
+    { text: "고객문의", link: "/contact" },
+  ];
 
   const NavLink = styled(Link)(({ theme }) => ({
     fontSize: "16px",
@@ -68,7 +45,7 @@ const Navbar = () => {
     alignItems: "center",
     justifyContent: "center",
     gap: theme.spacing(5),
-    [theme.breakpoints.down("md")]: {
+    [theme.breakpoints.down("750")]: {
       display: "none",
     },
   }));
@@ -82,19 +59,33 @@ const Navbar = () => {
     },
   }));
 
-  const NavbarContainer = styled(Container)(({ theme }) => ({
+  const NavbarContainer = styled(Box)(({ theme }) => ({
     position: "fixed",
-    background: "transparent",
+    zIndex: "1",
     top: "0",
     left: "0",
     right: "0",
     display: "flex",
     alignItems: "center",
+    margin: "0 150px 0 150px",
     justifyContent: "space-between",
     padding: theme.spacing(2),
-    [theme.breakpoints.down("md")]: {
-      padding: theme.spacing(2),
+    [theme.breakpoints.down("1080")]: {
+      margin: "0",
     },
+  }));
+
+  const NavbarWrapper = styled(Box)(({ theme }) => ({
+    position: "fixed",
+    zIndex: "1",
+    top: "0",
+    left: "0",
+    right: "0",
+    display: "flex",
+    height: "90px",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: theme.spacing(2),
   }));
 
   const NavbarLogo = styled("img")(({ theme }) => ({
@@ -124,42 +115,58 @@ const Navbar = () => {
   }));
 
   return (
-    <NavbarContainer>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-      >
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <NavbarLogo src={logoImg} alt="빠른상표" href="/" />{" "}
-          <NavbarLinksBox>
-            <NavLink to="/">신청내역</NavLink>
-            <NavLink to="/price">비용안내</NavLink>
-            <NavLink>절차안내</NavLink>
-            <NavLink>회사소개</NavLink>
-            <NavLink to="/review">등록후기</NavLink>
-            <NavLink>고객문의</NavLink>
-          </NavbarLinksBox>
+    <NavbarWrapper
+      backgroundColor={backgroundColor}
+      borderBottom={borderBottom}
+    >
+      <NavbarContainer>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "1rem",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Link to="/">
+              <NavbarLogo src={logoImg} alt="빠른상표" />
+            </Link>
+            <NavbarLinksBox>
+              <NavLink to="/price">비용안내</NavLink>
+              <NavLink>절차안내</NavLink>
+              <NavLink>회사소개</NavLink>
+              <NavLink>고객문의</NavLink>
+            </NavbarLinksBox>
+          </Box>
         </Box>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gaps: "1rem",
-        }}
-      >
-        <ButtonLink>상표등록 신청하기</ButtonLink>
-        <CustomMenuIcon onClick={toggleDrawer(true)} />
-        <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
-          {list("right")}
-        </Drawer>
-      </Box>
-    </NavbarContainer>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gaps: "1rem",
+          }}
+        >
+          <ButtonLink to="/trademarkRegistration">상표등록 신청하기</ButtonLink>
+          <CustomMenuIcon onClick={toggleDrawer(true)} />
+          <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+            <List>
+              {menuItems.map((item, index) => (
+                <ListItem key={index} component={Link} to={item.link}>
+                  <ListItemText primary={item.text} />
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+        </Box>
+      </NavbarContainer>
+    </NavbarWrapper>
   );
 };
 
