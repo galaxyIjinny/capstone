899f4b38f795b53171a998bfac155bb0eb4e6304
galaxyIjinny <yahyunjin5@naver.com>
2023-04-11 10:33:11 +0900

    merge reflection

--- src/main/java/com/example/demo/controller/RegisterController.java
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,10 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.*;
-import com.example.demo.service.CorporateService;
-import com.example.demo.service.MarkService;
-import com.example.demo.service.PersonalService;
-import com.example.demo.service.UserService;
+import com.example.demo.service.info.InfoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,64 +11,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/register")
 public class RegisterController {
     @Autowired
-    private MarkService markService;
-    @Autowired
-    private PersonalService personalService;
-    @Autowired
-    private CorporateService corporateService;
-    @Autowired
-    private UserService userService;
+    private InfoServiceImpl infoService;
 
     @PostMapping("/personal")
-    public ResponseEntity<RegisterDto> createPersonal(@RequestBody RegisterDto dto) {
-        MarkDto mark = markService.create(dto.getMark());
-        PersonalDto personal = personalService.create(mark.getId(), dto.getPersonal());
-        UserDto user = userService.create(mark.getId(), dto.getUser());
-
-        RegisterDto registerDto = RegisterDto.builder()
-                .mark(mark)
-                .personal(personal)
-                .user(user)
-                .build();
-        return ResponseEntity.status(HttpStatus.OK).body(registerDto);
+    public ResponseEntity<InfoDto> createPersonal(@RequestBody InfoDto dto) {
+        InfoDto infoDto = infoService.createPer(dto);
+        return ResponseEntity.status(HttpStatus.OK).body(infoDto);
     }
 
     @PostMapping("/corporate")
-    public ResponseEntity<RegisterDto> createCorporate(@RequestBody RegisterDto dto) {
-        MarkDto mark = markService.create(dto.getMark());
-        CorporateDto corporate = corporateService.create(mark.getId(), dto.getCorporate());
-        UserDto user = userService.create(mark.getId(), dto.getUser());
-
-        RegisterDto registerDto = RegisterDto.builder()
-                .mark(mark)
-                .corporate(corporate)
-                .user(user)
-                .build();
-        return ResponseEntity.status(HttpStatus.OK).body(registerDto);
+    public ResponseEntity<InfoDto> createCorporate(@RequestBody InfoDto dto) {
+        InfoDto infoDto = infoService.createCorp(dto);
+        return ResponseEntity.status(HttpStatus.OK).body(infoDto);
     }
-
-    /*@PostMapping("/register/complete")
-    public ResponseEntity<RegisterDto> createComplete(@RequestBody RegisterDto dto, @RequestParam("applicantType") String applicantType) {
-        MarkDto mark = markService.create(dto.getMark());
-        UserDto user = userService.create(mark.getId(), dto.getUser());
-        RegisterDto registerDto = new RegisterDto();
-
-        if (applicantType.equals("personal")) {
-            PersonalDto personal = personalService.create(mark.getId(), dto.getPersonal());
-            registerDto = RegisterDto.builder()
-                    .mark(mark)
-                    .personal(personal)
-                    .user(user)
-                    .build();
-        } else if (applicantType.equals("corporate")) {
-            CorporateDto corporate = corporateService.create(mark.getId(), dto.getCorporate());
-            registerDto = RegisterDto.builder()
-                    .mark(mark)
-                    .corporate(corporate)
-                    .user(user)
-                    .build();
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body(registerDto);
-    }*/
 }
